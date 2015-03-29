6e58ee2f6fcab007d15c4751afe1441afe0839f4
Lado Lomidze <lado.lomidze@gmail.com>
2015-03-29 03:17:55 +0200

    cleanup

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -28,17 +28,20 @@ APP.controller('ReposController', ['$scope', '$http', 'Loading', function($scope
 	$scope.tabs.repos.active = true;
 	$scope.tabs.gists.active = false;
 
-	$scope.repoUrl = 'https://api.github.com/users/Landish/repos?client_id=306c0c212db97ef0383f&client_secret=84027fe50405b0c4cf16943abf0e113710cd97ab';
+	$scope.repoUrl = 'https://api.github.com/users/Landish/repos';
 
 	$scope.load = function() {		
 
-		$http.get($scope.repoUrl, { cache: true }).success(function(data, status) {		
-			$scope.repos = data;			
+		$http.get($scope.repoUrl, { cache: true }).success(function(data) {
+
+            $scope.repos = data.filter(function(item) {
+                return item.fork == false;
+            });
+
 			Loading.hide();
 						
-		}).error(function(data, status) {			
-			Loading.message('Couldn\'t Load Repositories! <br /> Instead, please visit: <a href="https://github.com/Landish?tab=repositories" target="_blank">https://github.com/Landish</a>');			
-			//window.location.href = ('https://github.com/Landish?tab=repositories');
+		}).error(function() {
+			Loading.message('Couldn\'t Load Repositories! <br /> Instead, please visit: <a href="https://github.com/Landish?tab=repositories" target="_blank">https://github.com/Landish</a>');
 		});	
 	};
 
@@ -60,17 +63,20 @@ APP.controller('GistsController', ['$scope', '$http', 'Loading', function($scope
 	$scope.tabs.gists.active = true;
 	$scope.tabs.repos.active = false;
 
-	$scope.gistsUrl = 'https://api.github.com/users/Landish/gists?client_id=306c0c212db97ef0383f&client_secret=84027fe50405b0c4cf16943abf0e113710cd97ab';	
+	$scope.gistsUrl = 'https://api.github.com/users/Landish/gists';
 
 	$scope.load = function() {		
 
-		$http.get($scope.gistsUrl, { cache: true }).success(function(data, status) {		
-			$scope.gists = data;			
+		$http.get($scope.gistsUrl, { cache: true }).success(function(data) {
+
+            $scope.gists = data.filter(function(item) {
+                return item.description != null;
+            });
+
 			Loading.hide();					
 
-		}).error(function(data, status) {			
-			Loading.message('Couldn\'t Load Gists! <br /> Instead, please visit: <a href="https://gist.github.com/Landish" target="_blank">https://gist.github.com/Landish</a>');			
-			//window.location.href = ('https://gist.github.com/Landish');
+		}).error(function() {
+			Loading.message('Couldn\'t Load Gists! <br /> Instead, please visit: <a href="https://gist.github.com/Landish" target="_blank">https://gist.github.com/Landish</a>');
 		});	
 	};
 
